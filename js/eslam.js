a07ec6b43e0d0595b1ba1cfcfe6942c4bc734f5c
Solariii <eslamsolari1996@gmail.com>
2018-12-22 15:45:02 +0200

    file added

---
+++ js/eslam.js
@@ -0,0 +1,10 @@
+
+  function eslam()
+{
+document.getElementById("booktitle").innerHTML=document.getElementById("Book").value;
+document.getElementById("subtitle").innerHTML=document.getElementById("book-subtitle").value
+document.getElementById("author").innerHTML=document.getElementById("author-name").value;
+document.getElementById("publisher").innerHTML=document.getElementById("publishers").value;
+document.getElementById("year").innerHTML='@'+document.getElementById("publishing-year").value;
+document.getElementById("eslam_cover").style.background=document.getElementById("color").value;
+}
